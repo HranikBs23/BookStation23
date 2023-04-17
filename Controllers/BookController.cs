eed9e8c9d37ec385b710502b1c46ef2a5056dd46
hranikBs23 <Habibur.Rahman@brainstation-23.com>
2023-04-17 20:57:05 +0600

    Book Edit Added

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -118,5 +118,102 @@ namespace BookStation.Controllers
 
             return View(model);
         }
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.Include(b => b.Author).FirstOrDefault(b => b.BookId == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            var model = new EditBookViewModel
+            {
+                BookId = book.BookId,
+                Title = book.Title,
+                AuthorId = book.AuthorId,
+                Language = book.Language,
+                Description = book.Description,
+                Price = book.Price,
+                ExistingCoverImage = book.CoverImage,
+                Authors = _context.Authors.Select(a => new SelectListItem
+                {
+                    Value = a.AuthorId.ToString(),
+                    Text = a.Name
+                }).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditBookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var book = _context.Books.FirstOrDefault(b => b.BookId == model.BookId);
+
+                if (book == null)
+                {
+                    return NotFound();
+                }
+
+                book.Title = model.Title;
+                book.AuthorId = model.AuthorId;
+                book.Language = model.Language;
+                book.Description = model.Description;
+                book.Price = model.Price;
+
+                // Check if a cover image file was uploaded
+                if (model.NewCoverImage != null && model.NewCoverImage.Length > 0)
+                {
+                    // Get the file name and extension
+                    var fileName = Path.GetFileName(model.NewCoverImage.FileName);
+                    var fileExtension = Path.GetExtension(fileName);
+
+                    // Generate a unique file name
+                    var uniqueFileName = Guid.NewGuid().ToString() + fileExtension;
+
+                    // Set the image path to the wwwroot/images/books directory
+                    var imagePath = Path.Combine(_env.WebRootPath, "images", "books", uniqueFileName);
+
+                    // Create the directory if it does not exist
+                    Directory.CreateDirectory(Path.GetDirectoryName(imagePath));
+
+                    // Save the image file to the server
+                    using (var fileStream = new FileStream(imagePath, FileMode.Create))
+                    {
+                        await model.NewCoverImage.CopyToAsync(fileStream);
+                    }
+
+                    // Set the book's cover image property to the file path
+                    book.CoverImage = "/images/books/" + uniqueFileName;
+
+                    // If an existing cover image exists, delete it from the server
+                    if (!string.IsNullOrEmpty(model.ExistingCoverImage))
+                    {
+                        var existingImagePath = Path.Combine(_env.WebRootPath, model.ExistingCoverImage.TrimStart('/'));
+
+                        if (System.IO.File.Exists(existingImagePath))
+                        {
+                            System.IO.File.Delete(existingImagePath);
+                        }
+                    }
+                }
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            model.Authors = _context.Authors.Select(a => new SelectListItem
+            {
+                Value = a.AuthorId.ToString(),
+                Text = a.Name
+            }).ToList();
+
+            return View(model);
+        }
+
     }
 }
---
+++ Models/ViewModels/EditBookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStation.Models.ViewModels
+{
+    public class EditBookViewModel
+    {
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public string Language { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+        public IFormFile NewCoverImage { get; set; }
+
+        public string ExistingCoverImage { get; set; }
+        public List<SelectListItem> Authors { get; set; }
+    }
+}
